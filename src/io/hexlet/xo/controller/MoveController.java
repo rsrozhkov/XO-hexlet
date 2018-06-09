767276f3bfacd4c1801de6443345d0ad226324d5
rsrozhkov <rsrozhkov@yandex.ru>
2018-06-09 23:46:59 +0500

    MoveController - created

---
+++ src/io/hexlet/xo/controller/MoveController.java
@@ -0,0 +1,24 @@
+package io.hexlet.xo.controller;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
+import java.awt.*;
+
+public class MoveController {
+
+    public void applayFigure(final Field field,
+                             final Point point,
+                             final Figure figure) throws InvalidPointException,
+                                                        AlreadyOccupiedException {
+
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
+
+        field.setFigure(point, figure);
+
+    }
+}
