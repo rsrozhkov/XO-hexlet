3f733113413cf913fd54b77c7524892ee6672737
rsrozhkov <rsrozhkov@yandex.ru>
2018-06-12 19:56:15 +0500

    WinnerController - some fixes when testing

--- src/io/hexlet/xo/controller/WinnerController.java
+++ src/io/hexlet/xo/controller/WinnerController.java
@@ -45,14 +45,13 @@ public class WinnerController {
 
         try {
             currentFigure = field.getFigure(currentPoint);
+            if (currentFigure == null) return false;
             nextFigure = field.getFigure(nextPoint);
         } catch (final InvalidPointException e) {
             return true;
         }
 
-        if (currentFigure == null) return false;
-
-        if (currentFigure == nextFigure) return false;
+        if (currentFigure != nextFigure) return false;
 
         return check(field, nextPoint, pointGenerator);
 
